a093a3db06293813a21b134df5af9382a3fe8bb2
Lars Ekman <lars.g.ekman@est.tech>
2021-06-08 19:26:58 +0200

    Handle icmp dest unreach

--- src/lib/iputils.c
+++ src/lib/iputils.c
@@ -9,6 +9,15 @@
 #include <netinet/ip6.h>
 #include <netinet/icmp6.h>
 
+#ifdef VERBOSE
+#include <stdio.h>
+#define Dx(x) x
+#else
+#define Dx(x)
+#endif
+#define D(x)
+
+
 static unsigned ipv4TcpUdpHash(void const* data, unsigned len)
 {
 	// We hash on addresses and ports
@@ -17,7 +26,40 @@ static unsigned ipv4TcpUdpHash(void const* data, unsigned len)
 	hashData[0] = hdr->saddr;
 	hashData[1] = hdr->daddr;
 	hashData[2] = *((uint32_t*)data + hdr->ihl);
-	return HASH((uint8_t const*)hashData, sizeof(hashData));
+	return HASH(hashData, sizeof(hashData));
+}
+static uint32_t flip16(uint32_t v)
+{
+	uint16_t* p = (uint16_t*)&v;
+	return (p[0] << 16) + p[1];
+}
+static unsigned ipv4IcmpDestUnreachHash(void const* data, unsigned len)
+{
+	/*
+	  Dest unreachable including PMTU discovery reply. We must use the
+	  *inner* header to make sure the origial sender gets the reply.
+	*/
+	struct iphdr* hdr = (struct iphdr*)data;
+	struct icmphdr* ihdr = (struct icmphdr*)((uint32_t*)data + hdr->ihl);
+	D(printf(
+		   "ipv4IcmpDestUnreachHash; code=%u, mtu=%u\n",
+		   ihdr->code, ntohs(ihdr->un.frag.mtu)));
+
+	/*
+	  The original datagram is found on offset 8 in this icmp
+	  header. But since the original datagram is outgoing we must
+	  switch src<->dst for both addresses and port before we calculate
+	  the hash.
+	 */
+	hdr = (void*)ihdr + 8;
+	uint32_t hashData[3];
+	hashData[0] = hdr->daddr;
+	hashData[1] = hdr->saddr;
+	hashData[2] = flip16(*((uint32_t*)hdr + hdr->ihl));
+	D(printf(
+		   "  %u %08x %08x %08x\n", hdr->protocol,
+		   ntohl(hashData[0]), ntohl(hashData[1]), ntohl(hashData[2])));
+	return HASH(hashData, sizeof(hashData));
 }
 static unsigned ipv4IcmpHash(void const* data, unsigned len)
 {
@@ -26,14 +68,14 @@ static unsigned ipv4IcmpHash(void const* data, unsigned len)
 	int32_t hashData[3];
 	hashData[0] = hdr->saddr;
 	hashData[1] = hdr->daddr;
+	Dx(printf("ipv4IcmpHash; type=%u\n", ihdr->type));
 	switch (ihdr->type) {
 	case ICMP_ECHO:
 		// We hash on addresses and id
 		hashData[2] = ihdr->un.echo.id;
 		break;
-	case ICMP_FRAG_NEEDED:
-		// This is a PMTU discovery reply. We must use the *inner*
-		// header to make sure the origial sender gets the reply.
+	case ICMP_DEST_UNREACH:
+		return ipv4IcmpDestUnreachHash(data, len);
 	default:
 		hashData[2] = 0;
 	}
@@ -82,7 +124,7 @@ ipv6TcpUdpHash(struct ip6_hdr const* h, uint32_t const* ports)
 	int32_t hashData[9];
 	memcpy(hashData, &h->ip6_src, 32);
 	hashData[8] = *ports;
-	return HASH((uint8_t const*)hashData, sizeof(hashData));
+	return HASH(hashData, sizeof(hashData));
 }
 static unsigned
 ipv6IcmpHash(struct ip6_hdr const* h, struct icmp6_hdr const* ih)
@@ -90,7 +132,7 @@ ipv6IcmpHash(struct ip6_hdr const* h, struct icmp6_hdr const* ih)
 	int32_t hashData[9];
 	memcpy(hashData, &h->ip6_src, 32);
 	hashData[8] = ih->icmp6_id;
-	return HASH((uint8_t const*)hashData, sizeof(hashData));
+	return HASH(hashData, sizeof(hashData));
 }
 
 unsigned ipv6Hash(
