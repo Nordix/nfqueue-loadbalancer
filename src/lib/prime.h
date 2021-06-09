64634e0f21f5a53f147a9d10aebed217c1448ae9
Lars Ekman <lars.g.ekman@est.tech>
2021-06-09 10:51:28 +0200

    Make primeBelow() a library function

---
+++ src/lib/prime.h
@@ -0,0 +1,11 @@
+#pragma once
+/*
+  SPDX-License-Identifier: Apache-2.0
+  Copyright (c) 2021 Nordix Foundation
+*/
+
+/*
+  Return a prime below n.
+  if n >= 994009 then 994009 is always returned.
+ */
+unsigned primeBelow(unsigned n);
